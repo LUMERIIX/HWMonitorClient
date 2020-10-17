0da766111548ca9cb7b8c128e1a4397570b1b6f1
LUMERIIX <lukas.jenni@hotmail.de>
2020-10-17 22:18:25 +0200

    clean up + add Processing Timeline (1s)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,50 +1,63 @@
 package org.example;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-import eu.hansolo.tilesfx.Tile;
-import eu.hansolo.tilesfx.Tile.SkinType;
-import eu.hansolo.tilesfx.TileBuilder;
-import eu.hansolo.medusa.GaugeBuilder;
-import eu.hansolo.medusa.Gauge;
-import eu.hansolo.medusa.Section;
-
-import static javafx.scene.paint.Color.*;
+import javafx.util.Duration;
+import java.util.Timer;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
+    private long lastTimerCall;
+    private Timer timer;
+    private long test = 0;
+
+    private void sysCycle ()
+    {
+        test = test + 10;
+        cpu.MainCpuTemp.setValue(test);
+    }
+
+    private Timeline processTimeline = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>()
+            {
+                @Override
+                public void handle(ActionEvent event)
+                {
+                    System.out.println("this is called every 1 seconds on UI thread");
+                    sysCycle();
+                }
+            }
+        )
+    );
+
+    private CPU cpu;
+    private RAM ram;
+    private GPU gpu;
+
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage)
+    {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
 
-
         var gridPane = Dashboard.createDashboardGridPane();
 
-        CPU cpu = new CPU();
-        RAM ram = new RAM();
-        GPU gpu = new GPU();
+        cpu = new CPU();
+        ram = new RAM();
+        gpu = new GPU();
         Storage storage = new Storage();
         Interfaces interfaces = new Interfaces();
         FanController fans = new FanController();
-
 
         gridPane.add(cpu.MainCpuTemp,0,0);
         gridPane.add(ram.MainRamUsage,1,0);
@@ -57,13 +70,14 @@ public class App extends Application {
 
         stage.setScene(scene);
         stage.show();
+
+        processTimeline.setCycleCount(Timeline.INDEFINITE);
+        processTimeline.play();
     }
 
     public static void main(String[] args)
     {
         launch();
-        
-
     }
 
 }
