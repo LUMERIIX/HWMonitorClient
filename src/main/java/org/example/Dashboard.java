836d424486344d4cc55b1d179e5cfc2e3814df28
LUMERIIX <lukas.jenni@hotmail.de>
2020-10-17 12:55:11 +0200

    add global function to create tables for component informations (three columns format with unit)

--- src/main/java/org/example/Dashboard.java
+++ src/main/java/org/example/Dashboard.java
@@ -48,7 +48,7 @@ public class Dashboard
     {
         Label[] label = new Label[NrOfLabels];
 
-        for(int i = 0; i < text.length; i++)
+        for(int i = 0; i < NrOfLabels; i++)
         {
             label[i] = new Label();
             //label[i].setPrefSize(TILE_WIDTH/2,10);
@@ -72,7 +72,28 @@ public class Dashboard
         HBox.setHgrow(middleSpacer, Priority.ALWAYS);
         HBox.setHgrow(ColumnOne, Priority.NEVER);
         HBox.setHgrow(ColumnTwo, Priority.NEVER);
-        HBox hbox = new HBox(5, sideSpacerLeft,ColumnOne, middleSpacer, ColumnTwo,sideSpacerRight);
+        HBox hbox = new HBox(5, sideSpacerLeft,ColumnOne, middleSpacer, ColumnTwo, sideSpacerRight);
+        hbox.setAlignment(Pos.CENTER_LEFT);
+        hbox.setFillHeight(true);
+
+        return hbox;
+    }
+
+    public static HBox createHBox (Label ColumnOne, Label ColumnTwo, Label ColumnThree)
+    {
+        Region middleSpacer = new Region();
+        Region sideSpacerLeft = new Region();
+        Region sideSpacerRight = new Region();
+        sideSpacerLeft.setPrefSize(20, 20);
+        sideSpacerRight.setPrefSize(20, 20);
+        middleSpacer.setPrefSize(5, 5);
+        HBox.setHgrow(sideSpacerLeft, Priority.NEVER);
+        HBox.setHgrow(sideSpacerRight, Priority.NEVER);
+        HBox.setHgrow(middleSpacer, Priority.ALWAYS);
+        HBox.setHgrow(ColumnOne, Priority.NEVER);
+        HBox.setHgrow(ColumnTwo, Priority.NEVER);
+        HBox.setHgrow(ColumnThree,Priority.NEVER);
+        HBox hbox = new HBox(5, sideSpacerLeft,ColumnOne, middleSpacer, ColumnTwo, ColumnThree, sideSpacerRight);
         hbox.setAlignment(Pos.CENTER_LEFT);
         hbox.setFillHeight(true);
 
@@ -119,6 +140,57 @@ public class Dashboard
             HBox hbox = createHBox(compLabels[i],valueLabels[i]);
             hBoxList.add(hbox);
 
+        }
+
+        for(int i = 0; i < hBoxList.size(); i++)
+            tabel.getChildren().add(hBoxList.get(i));
+
+        return tabel;
+    }
+
+    public static VBox createComponentTable (String [] ColumnNames, String [] CompName, int [] Value, String unit)
+    {
+        List<HBox> hBoxList = new ArrayList<HBox>();
+        VBox tabel = new VBox();
+        String[] val = new String[Value.length];
+        Label[] compLabels = new Label[Value.length];
+        Label[] valueLabels = new Label[Value.length];
+        Label[] unitLabels = new Label[Value.length+1];
+        String[] units = new String[Value.length];
+
+        Region spacer = new Region();
+        spacer.setPrefSize(5,5);
+
+
+        tabel.setPrefSize(TILE_WIDTH,TILE_HEIGHT);
+
+        for(int i = 0; i < Value.length; i++)
+        {
+            val[i] = String.valueOf(Value[i]);
+            units[i] = unit;
+        }
+
+        Label columnLabelLeft = new Label(ColumnNames[0]);
+        Label columnLabelRight = new Label(ColumnNames[1]);
+
+
+        columnLabelLeft.setAlignment(Pos.CENTER_LEFT);
+        columnLabelLeft.setTextFill(Text);
+        columnLabelRight.setAlignment(Pos.CENTER_RIGHT);
+        columnLabelRight.setTextFill(Text);
+
+
+        compLabels = createLabelArray(CompName.length,Pos.CENTER_LEFT,CompName);
+        valueLabels = createLabelArray(Value.length,Pos.CENTER_RIGHT,val);
+        unitLabels = createLabelArray(Value.length,Pos.CENTER_RIGHT,units);
+
+        //HBox hbox = new HBox();
+        hBoxList.add(createHBox(columnLabelLeft,columnLabelRight));
+
+        for(int i = 0; i < CompName.length; i++)
+        {
+            HBox hbox = createHBox(compLabels[i],valueLabels[i],unitLabels[i]);
+            hBoxList.add(hbox);
         }
 
         for(int i = 0; i < hBoxList.size(); i++)
