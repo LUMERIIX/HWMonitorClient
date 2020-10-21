e8aafd6caabca62fc03950f4bf2c3655c01235f1
LUMERIIX <lukas.jenni@hotmail.de>
2020-10-21 21:05:23 +0200

    update visual style of main ram gauge

--- src/main/java/org/example/RAM.java
+++ src/main/java/org/example/RAM.java
@@ -5,6 +5,7 @@ import eu.hansolo.medusa.GaugeBuilder;
 import eu.hansolo.medusa.Section;
 import eu.hansolo.tilesfx.Tile;
 import eu.hansolo.tilesfx.TileBuilder;
+import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 
 
@@ -25,26 +26,23 @@ public class RAM
 
     private void createMainTile()
     {
-        Gauge ramGauge = GaugeBuilder.create()
+        ramGauge = GaugeBuilder.create()
                 .skinType(Gauge.SkinType.SIMPLE_SECTION)
                 .prefSize(Dashboard.TILE_WIDTH, Dashboard.TILE_WIDTH)
+                .sections(new Section(0, 33, Color.LIME),
+                        new Section(33, 66, Color.YELLOW),
+                        new Section(66, 100, Color.CRIMSON))
                 .animated(true)
                 .unit("%")
                 .valueColor(Tile.FOREGROUND)
                 .titleColor(Tile.FOREGROUND)
                 .unitColor(Tile.FOREGROUND)
-                .barColor(Tile.BLUE)
-                .needleColor(Tile.FOREGROUND)
-                .barColor(Tile.BLUE)
-                .barBackgroundColor(Tile.BACKGROUND.darker())
-                .tickLabelColor(Tile.FOREGROUND)
                 .majorTickMarkColor(Tile.FOREGROUND)
                 .minorTickMarkColor(Tile.FOREGROUND)
                 .mediumTickMarkColor(Tile.FOREGROUND)
                 .build();
 
         ramGauge.setBarColor(Tile.FOREGROUND);
-        ramGauge.setSections(new Section(66, 100, Tile.BLUE));
         MainRamUsage  = TileBuilder.create()
                 .prefSize(Dashboard.TILE_WIDTH, Dashboard.TILE_WIDTH)
                 .skinType(Tile.SkinType.CUSTOM)
